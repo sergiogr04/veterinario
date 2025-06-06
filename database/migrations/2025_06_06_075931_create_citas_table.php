73b3fff1eb82f9d03e752eb6dbeee0fc8a79c39e
Sergio Gómez <s.gomez@dssnetwork.es>
2025-06-06 12:52:08 +0200

    puede ser el ultimo?

--- database/migrations/2025_06_06_075931_create_citas_table.php
+++ database/migrations/2025_06_06_075931_create_citas_table.php
@@ -8,7 +8,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::create('citas', function (Blueprint $table) {
-            $table->id();  
+            $table->id('id_cita'); 
             $table->date('fecha');
             $table->time('hora');
             $table->string('tipo');
@@ -16,10 +16,13 @@ return new class extends Migration {
             $table->string('estado');
             $table->unsignedBigInteger('id_mascota');
             $table->unsignedBigInteger('id_cliente');
+        
             $table->foreign('id_mascota')->references('id_mascota')->on('mascotas')->onDelete('cascade');
             $table->foreign('id_cliente')->references('id_usuario')->on('usuarios')->onDelete('cascade');
+        
             $table->timestamps();
         });
+        
     }
 
     public function down(): void
