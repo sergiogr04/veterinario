18291d8acff645bae4aaa380f9955d742d587fec
Sergio Gómez <s.gomez@dssnetwork.es>
2025-06-06 11:02:07 +0200

    cambios en migraciones y seeders

--- database/migrations/2025_06_06_080133_create_usuarios_table.php
+++ database/migrations/2025_06_06_080133_create_usuarios_table.php
@@ -9,14 +9,14 @@ return new class extends Migration {
     {
         Schema::create('usuarios', function (Blueprint $table) {
             $table->id();  // Laravel estándar para clave primaria
-                        $table->string('dni', 9);
+            $table->string('dni', 9);
             $table->string('nombre', 50);
             $table->string('apellidos', 100);
             $table->string('direccion', 100);
             $table->string('telefono', 15);
             $table->string('email', 100);
             $table->string('password', 255);
-            $table->string('rol'); // original enum
+            $table->string('rol'); 
             $table->string('remember_token', 100);
             $table->timestamps();
         });
