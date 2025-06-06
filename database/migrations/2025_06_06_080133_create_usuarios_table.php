c0645e5776af3a58f8df3444e433bda394c9c23f
Sergio Gómez <s.gomez@dssnetwork.es>
2025-06-06 11:13:45 +0200

    migraciones de usuario

--- database/migrations/2025_06_06_080133_create_usuarios_table.php
+++ database/migrations/2025_06_06_080133_create_usuarios_table.php
@@ -8,16 +8,16 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::create('usuarios', function (Blueprint $table) {
-            $table->id();  // Laravel estándar para clave primaria
-            $table->string('dni', 9);
+            $table->id();
+            $table->string('dni', 9)->unique();
             $table->string('nombre', 50);
             $table->string('apellidos', 100);
-            $table->string('direccion', 100);
+            $table->string('direccion')->nullable();
             $table->string('telefono', 15);
             $table->string('email', 100);
             $table->string('password', 255);
             $table->string('rol'); 
-            $table->string('remember_token', 100);
+            $table->rememberToken();
             $table->timestamps();
         });
     }
