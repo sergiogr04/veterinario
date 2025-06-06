1729c2380e81adc03220f582a4fcd6a14e2673d5
Sergio Gómez <s.gomez@dssnetwork.es>
2025-06-06 12:17:38 +0200

    revert

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -9,7 +9,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
 use App\Providers\RouteServiceProvider;
-use Illuminate\Support\Facades\Log;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -27,19 +26,12 @@ class AuthenticatedSessionController extends Controller
     public function store(LoginRequest $request): RedirectResponse
     {
         $request->authenticate();
+
         $request->session()->regenerate();
-    
-        Log::info('Login ejecutado', [
-            'check' => Auth::check(),
-            'user' => Auth::user(),
-            'session_id' => session()->getId(),
-            'ip' => request()->ip(),
-            'user_agent' => request()->userAgent()
-        ]);
-    
-        return redirect()->intended(RouteServiceProvider::redirectByRole(Auth::user()));
+
+        $user = Auth::user();
+        return redirect()->intended(RouteServiceProvider::redirectByRole($user));
     }
-    
 
     /**
      * Destroy an authenticated session.
